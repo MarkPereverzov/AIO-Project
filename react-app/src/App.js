da00e3604fcf2b17e1d5939f071b4b215c717e84
Mark <mark.pereverzov@gmail.com>
2024-08-21 19:06:14 +0300

    update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {HashRouter, Routes, Route, NavLink} from 'react-router-dom';
-import { useState } from 'react';
+import { Header } from './Header';
 
 function Card(props){
   return (
@@ -16,25 +16,40 @@ function Card(props){
   );
 }
 
+function AddCard(props){
+  return (
+    <NavLink to={`/${props.address}`}>
+      <div class="card">
+        <div class="content">
+          <img src={`./img/${props.image}.png`}/>
+          <h1>{props.title}</h1>
+        </div>
+      </div>
+    </NavLink>
+  );
+}
+
 function Main() {
   return (
-    <div>
-      <penis class="penis">
-        <Card address="NoSmoking" image="question" title="Сколько я держусь без курения?" text="Давай проверим твою выдержку и подсчитаем, сколько денег ты сумел сэкономить."></Card>
-        <Card address="Dream" image="question" title="Что с моим сном?" text="Сон играет важную роль в жизни. Пора выяснить, насколько крепок твой сон."></Card>
-        <Card address="Budget" image="question" title="На что уходит мой бюджет?" text="Считается некрасивым заглядывать в чужой кошелек, но с твоего разрешения мы это все-таки сделаем."></Card>
-        <Card address="Nutrion" image="question" title="Чем же я питаюсь?" text="Пришло время обратить внимание на питание, и мы поможем тебе в этом."></Card>
-        <Card address="Job" image="question" title="Мне пора на работу?" text="Теперь тебе не нужно беспокоиться о том, что опоздаешь на работу — мы напомним тебе вовремя."></Card>
-        <Card address="NoSmoking" image="question" title="Сколько я воздерживаюсь?" text="Воздержание — личное дело каждого, и мы обещаем сохранить это в тайне."></Card>
-      </penis>
-    </div>
+    <>
+    <Header></Header>
+      <div class="CardBoard">
+        <Card address="NoSmoking" image="smoking" title="Сколько я держусь без курения?" text="Давай проверим твою выдержку и подсчитаем, сколько денег ты сумел сэкономить."></Card>
+        <Card address="Dream" image="dream" title="Что с моим сном?" text="Сон играет важную роль в жизни. Пора выяснить, насколько крепок твой сон."></Card>
+        <Card address="Budget" image="budget" title="На что уходит мой бюджет?" text="Считается некрасивым заглядывать в чужой кошелек, но с твоего разрешения мы это все-таки сделаем."></Card>
+        <Card address="Nutrion" image="food" title="Чем же я питаюсь?" text="Пришло время обратить внимание на питание, и мы поможем тебе в этом."></Card>
+        <Card address="Job" image="job" title="Мне пора на работу?" text="Теперь тебе не нужно беспокоиться о том, что опоздаешь на работу — мы напомним тебе вовремя."></Card>
+        <AddCard address="NoSmoking" image="plus" title="Добавить?"></AddCard>
+      </div>
+    </>
   );
 }
 
 function NoSmoking() {
   return (
     <div>
-      <penis class="penis">
+      <Header></Header>
+      <penis class="CardBoard">
         <h1>NoSmoking</h1>
         <NavLink to='/'>Go to Main</NavLink>
       </penis>
@@ -45,7 +60,7 @@ function NoSmoking() {
 function Dream() {
   return (
     <div>
-      <penis class="penis">
+      <penis class="CardBoard">
         <h1>Dream</h1>
         <NavLink to='/'>Go to Main</NavLink>
       </penis>
@@ -56,7 +71,7 @@ function Dream() {
 function Budget() {
   return (
     <div>
-      <penis class="penis">
+      <penis class="CardBoard">
         <h1>Budget</h1>
         <NavLink to='/'>Go to Main</NavLink>
       </penis>
@@ -67,7 +82,7 @@ function Budget() {
 function Nutrion() {
   return (
     <div>
-      <penis class="penis">
+      <penis class="CardBoard">
         <h1>Nutrion</h1>
         <NavLink to='/'>Go to Main</NavLink>
       </penis>
@@ -78,7 +93,7 @@ function Nutrion() {
 function Job() {
   return (
     <div>
-      <penis class="penis">
+      <penis class="CardBoard">
         <h1>Job</h1>
         <NavLink to='/'>Go to Main</NavLink>
       </penis>
---
+++ react-app/src/Header.js
@@ -0,0 +1,21 @@
+import './index.css';
+import {HashRouter, Routes, Route, NavLink} from 'react-router-dom';
+
+export function Header(props){
+    return (
+        <div class="header">
+            <div class="header-content">
+                <img src='./img/menu.png'/>
+            </div>
+
+            <div class="header-content">
+                <h1>Budget</h1>
+            </div>
+
+            <div class="header-content">
+                <img src='./img/clock.png'/>
+                <img src='./img/avatar.png'/>
+            </div>
+        </div>
+    );
+}
