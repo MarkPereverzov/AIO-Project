1acf13a34e2c55953b242bbb7558b9ce6fe7e77c
OzzeY72 <nabacap72@gmail.com>
2024-08-23 00:23:08 +0200

    Create minicalendar component

---
+++ react-app/src/components/widgets/MiniCalendar/MiniCalendar.js
@@ -0,0 +1,7 @@
+export default MiniCalendar() 
+{
+    //TODO
+    return (
+        <div></div>
+    );
+}
--- react-app/src/components/widgets/index.js
+++ react-app/src/components/widgets/index.js
@@ -2,5 +2,6 @@ import AddCard from "./AddCard/AddCard";
 import Card from "./Card/Card";
 import CardBoard from "./CardBoard/CardBoard";
 import Header from "./Header/Header";
+import MiniCalendar from "./MiniCalendar/MiniCalendar";
 
-export {AddCard, Card, CardBoard, Header};
+export {AddCard, Card, CardBoard, Header, MiniCalendar};
