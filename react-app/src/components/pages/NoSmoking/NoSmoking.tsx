038d28366f016192a725fd94ae3af789ebaa405e
OzzeY72 <nabacap72@gmail.com>
2024-08-24 00:43:10 +0200

    scaffold MiniCalendar

--- react-app/src/components/pages/NoSmoking/NoSmoking.tsx
+++ react-app/src/components/pages/NoSmoking/NoSmoking.tsx
@@ -6,7 +6,11 @@ export default function NoSmoking() {
       <>
         <Header title="Сколько я держусь без курения?"></Header>
         <div className='page-container'>
-          <MiniCalendar></MiniCalendar>
+          <div className="smoke">
+            <MiniCalendar shift={1}></MiniCalendar>
+            <MiniCalendar shift={2}></MiniCalendar>
+            <MiniCalendar shift={3}></MiniCalendar>
+          </div>
           <NavLink to='/'><h1>Вернуться назад</h1></NavLink>
         </div>
       </>
--- react-app/src/components/widgets/MiniCalendar/MiniCalendar.tsx
+++ react-app/src/components/widgets/MiniCalendar/MiniCalendar.tsx
@@ -1,12 +1,16 @@
 import MiniCalendarDay from "../MiniCalendarDay/MiniCalendarDay";
 import "./MiniCalendar.css"
 
-export default function MiniCalendar() 
+interface Props {
+    shift: number,
+}
+
+export default function MiniCalendar(props: Props) 
 {
     let MC_days = [];
     for(let i = 1;i <= 31;i++)
     {
-        MC_days.push(<MiniCalendarDay day={i} color="#0000aa" />);
+        MC_days.push(<MiniCalendarDay day={i} color="#0000aa" shift={props.shift}/>);
     }
     //TODO
     return (
