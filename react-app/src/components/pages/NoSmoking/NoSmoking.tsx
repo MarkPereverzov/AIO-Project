43dbae3c8c7176e4ad872e5344ac75f7012caa08
OzzeY72 <nabacap72@gmail.com>
2024-08-24 21:02:07 +0200

    Calendar+

--- react-app/src/components/pages/NoSmoking/NoSmoking.tsx
+++ react-app/src/components/pages/NoSmoking/NoSmoking.tsx
@@ -2,14 +2,13 @@ import { NavLink } from 'react-router-dom';
 import { Header, MiniCalendar } from '../../widgets'
 
 export default function NoSmoking() {
+
     return (
       <>
         <Header title="Сколько я держусь без курения?"></Header>
         <div className='page-container'>
           <div className="smoke">
             <MiniCalendar shift={1}></MiniCalendar>
-            <MiniCalendar shift={2}></MiniCalendar>
-            <MiniCalendar shift={3}></MiniCalendar>
           </div>
           <NavLink to='/'><h1>Вернуться назад</h1></NavLink>
         </div>
--- react-app/src/components/widgets/MiniCalendar/MiniCalendar.tsx
+++ react-app/src/components/widgets/MiniCalendar/MiniCalendar.tsx
@@ -1,21 +1,52 @@
 import MiniCalendarDay from "../MiniCalendarDay/MiniCalendarDay";
+import { useState } from 'react';
 import "./MiniCalendar.css"
 
 interface Props {
     shift: number,
 }
 
+interface MiniCalendarState {
+	date: Date,
+};
+
 export default function MiniCalendar(props: Props) 
 {
+    const [state, setState] = useState<MiniCalendarState>({date:new Date("2024-02-01")});
+
+    console.log(findlastDay(state.date));
+
     let MC_days = [];
-    for(let i = 1;i <= 31;i++)
+    for(let i = 1;i <= findlastDay(state.date);i++)
     {
-        MC_days.push(<MiniCalendarDay day={i} color="#ffffff" shift={props.shift}/>);
+        MC_days.push(<MiniCalendarDay day={i} color="#ffffff" shift={findShift(state.date)}/>);
     }
+
     //TODO
     return (
-        <div className="minicalendar-container">
-            {MC_days}
+        <div className="minicalendar">
+            <div className="minicalendar-legend">
+                <h1 style={{gridColumnStart:1}}>ПН</h1>
+                <h1 style={{gridColumnStart:2}}>ВТ</h1>
+                <h1 style={{gridColumnStart:3}}>СР</h1>
+                <h1 style={{gridColumnStart:4}}>ЧТ</h1>
+                <h1 style={{gridColumnStart:5}}>ПТ</h1>
+                <h1 style={{gridColumnStart:6}}>СБ</h1>
+                <h1 style={{gridColumnStart:7}}>НД</h1>
+            </div>
+            <div className="minicalendar-container">
+                {MC_days}
+            </div>
         </div>
     );
+}
+
+function findlastDay(date: Date) {
+    console.log(date.getMonth());
+    //date.setDate(0);
+    return(date.getDate());
+}
+
+function findShift(date: Date) {
+    return(new Date(date.getFullYear(), date.getMonth(), 1).getDay()-1);
 }
