455ef59c2ba4498e74ba929b098f47114ad1cfeb
OzzeY72 <nabacap72@gmail.com>
2024-08-25 20:22:44 +0200

    complete minicalendar

--- react-app/src/components/widgets/MiniCalendar/MiniCalendar.tsx
+++ react-app/src/components/widgets/MiniCalendar/MiniCalendar.tsx
@@ -1,5 +1,8 @@
 import MiniCalendarDay from "../MiniCalendarDay/MiniCalendarDay";
-import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { faCaretLeft } from '@fortawesome/free-solid-svg-icons'
+import { ReactEventHandler, useState } from 'react';
 import "./MiniCalendar.css"
 
 interface Props {
@@ -9,7 +12,7 @@ interface Props {
 interface MiniCalendarState {
 	date: Date,
 };
-
+//5.5.4
 export default function MiniCalendar(props: Props) 
 {
     const [state, setState] = useState<MiniCalendarState>({date:new Date()});
@@ -25,7 +28,9 @@ export default function MiniCalendar(props: Props)
     return (
         <div className="minicalendar" style={{width:"320px"}}>
             <div className="minicalendar-date">
+                <FontAwesomeIcon onClick={()=>changeDate('decrease',state,setState)} icon={faCaretLeft} />
                 <h1 style={{textAlign:"center"}}>{state.date.toLocaleString('default',{month: 'long',year: "numeric", timeZone:'UTC'})}</h1>
+                <FontAwesomeIcon onClick={()=>changeDate('increase',state,setState)} icon={faCaretRight} />
             </div>
             <div className="minicalendar-legend">
                 <h1 style={{gridColumnStart:1}}>ПН</h1>
@@ -41,6 +46,16 @@ export default function MiniCalendar(props: Props)
             </div>
         </div>
     );
+}
+
+function changeDate(direction: string, state: MiniCalendarState ,setState: (state: MiniCalendarState) => void) {
+    let date = new Date(state.date);
+    switch (direction) {
+        case "decrease": date.setMonth(state.date.getMonth()-1); break;
+        case "increase": date.setMonth(state.date.getMonth()+1); break;
+        default: break;
+    }
+    setState({date: date})
 }
 
 function findlastDay(date: Date) {
--- react-app/src/components/widgets/MiniCalendarDay/MiniCalendarDay.tsx
+++ react-app/src/components/widgets/MiniCalendarDay/MiniCalendarDay.tsx
@@ -8,8 +8,11 @@ interface Props {
 }
 
 export default function MiniCalendarDay(props: Props) {
+    console.log(props.shift);
+    let shift = props.shift;
+    if(props.shift == 0) shift = 7;
     return (
-        <div className="minicalendar-day" style={{gridColumnStart:(props.day-1+props.shift-1)%7+1}}>
+        <div className="minicalendar-day" style={{gridColumnStart:(props.day+shift-2)%7+1}}>
             <h1 className='minicalendar-day-number' style={{color:props.color}}>{props.day}</h1>
         </div>
     );
