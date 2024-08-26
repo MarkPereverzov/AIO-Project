eed5e6514445cf42e8c6dca8ac53ae2fce6c003f
Mark <mark.pereverzov@gmail.com>
2024-08-26 16:02:45 +0300

    The last two issues have been completed

--- react-app/src/components/pages/BadHabits/BadHabits.tsx
+++ react-app/src/components/pages/BadHabits/BadHabits.tsx
@@ -1,11 +1,16 @@
-import { Header, MiniCalendar, CardBoardBadHabits, SideMenu} from '../../widgets'
+import { Header, Card} from '../../widgets'
 import "./BadHabits.css"
 
 export default function BadHabits() {
   return (
     <div className="main-container">
       <Header title=" Какие у меня зависимости?"></Header>
-      <CardBoardBadHabits></CardBoardBadHabits>
+
+      <div className="CardBoard-BadHabits">
+        <Card address="Smoking" image="smoking" title="Курение" text="Сильный кашель, хрипы и накопление мокроты мешают нормально жить, пора что-то менять."></Card>
+        <Card address="Alcohol" image="bad-habits" title="Алкоголь" text="Алкоголь является одной из самых распространенных зависимостей в мире, настало время освободиться от него."></Card>
+        <Card address="Sweet" image="candy" title="Сладкое" text="Избыток глюкозы, высокий уровень инсулина и лишний вес — это точно не то, что тебе нужно."></Card>
+      </div>
     </div>
   );
 }
--- react-app/src/components/widgets/index.tsx
+++ react-app/src/components/widgets/index.tsx
@@ -1,10 +1,8 @@
 import AddCard from "./AddCard/AddCard";
 import Card from "./Card/Card";
-import CardBoardMain from "./CardBoards/CardBoard-Main";
-import CardBoardBadHabits from "./CardBoards/CardBoard-BadHabits";
 import Header from "./Header/Header";
 import Information from "./Information/Information";
 import MiniCalendar from "./MiniCalendar/MiniCalendar";
 import SideMenu from "./SideMenu/SideMenu";
 
-export {AddCard, Card, CardBoardMain, CardBoardBadHabits, Header, Information, MiniCalendar, SideMenu};
+export {AddCard, Card, Header, Information, MiniCalendar, SideMenu};
