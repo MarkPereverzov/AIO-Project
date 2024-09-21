c0d73ff90f11bd985d6ebee59690e36332b4ffd6
Mark <mark.pereverzov@gmail.com>
2024-09-21 12:27:33 +0300

    New pages: JobPage, NutrionPage

--- src/features/playModal/ui/PlayModal.tsx
+++ src/features/playModal/ui/PlayModal.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { HealthStreakResponseDto } from '@/shared/api';
+import '../PlayModal.module.css';
 
 interface PlayModalProps {
   show: boolean;
@@ -10,18 +11,24 @@ interface PlayModalProps {
   handleStreak: () => void;
 }
 
-//Пометка для Марка: Мы так не делаем, проверять кнопку на её текст это полный бред
 export const PlayModal = ({ show, status, isLoading, handleClose, handleStreak }: PlayModalProps) => {
   const isStarted = status?.isExist;
   const buttonText = isStarted ? 'остановить' : 'начать';
 
   return (
-    <Modal show={show} onHide={handleClose}>
+    <Modal
+      show={show}
+      onHide={handleClose}
+      centered // Этот проп делает модальное окно центрированным
+    >
       <Modal.Header closeButton>
         <Modal.Title>Действие</Modal.Title>
       </Modal.Header>
-      <Modal.Body style={{ borderBottom: 'none' }}>
-        <Button variant="primary" onClick={handleStreak}>
+      <Modal.Body
+        style={{
+          
+        }} className='bodyModal'>
+        <Button variant="primary" onClick={handleStreak} className='buttonModal'>
           {buttonText}
         </Button>
       </Modal.Body>
