35f9bfb2e050a64d69efa9748c40326f46ea08ab
Maksym Vrutskyi <nabacap72@gmail.com>
2024-11-16 23:04:04 +0100

    Health page rework

--- src/cpages/health/ui/HealthPage.tsx
+++ src/cpages/health/ui/HealthPage.tsx
@@ -1,16 +1,21 @@
 import { Header } from '@/widgets/header';
 import { Block } from '@/widgets/block';
 import { FaSmoking, FaWineBottle, FaCoffee , FaCandyCane } from 'react-icons/fa'; // Импортируем иконки
+import { LiaSmokingBanSolid } from "react-icons/lia";
+import { PiBeerBottle, PiCoffee  } from "react-icons/pi";
+import { TbCandy } from "react-icons/tb";
+import { BiInjection } from "react-icons/bi";
 import { TbRating18Plus } from "react-icons/tb";
+
 export const HealthPage = () => {
   
   const blocks = [
-    { title: 'Курение', subtitle: 'Сильный кашель, хрипы и накопление мокроты мешают нормально жить, пора что-то менять.', Icon: FaSmoking, href: '/health/smoking' },
-    { title: 'Алкоголь', subtitle: 'Алкоголь является одной из самых распространенных зависимостей в мире, время освободиться от него.', Icon: FaWineBottle, href: '/dream' },
-    { title: 'Кофе', subtitle: 'Сильный кашель, хрипы и накопление мокроты мешают нормально жить, пора что-то менять.', Icon: FaCoffee, href: '/dream' },
-    { title: 'Сладкое', subtitle: 'Избыток глюкозы, высокий уровень инсулина и лишний вес — это точно не то, что тебе нужно.', Icon: FaCandyCane, href: '/dream' },
-    { title: 'Наркотики', subtitle: 'Они приводят к потере контроля над собственной жизнью. Это не то, что пойдет тебе на пользу.', Icon: FaCoffee, href: '/dream' },
-    { title: 'Порно', subtitle: 'Это иллюзия, которая искажает восприятие реальных отношений и сексуальности. Время избавиться от неё.', Icon: TbRating18Plus, href: '/dream' },
+    { title: 'Курение', subtitle: 'Сильный кашель, хрипы и накопление мокроты мешают нормально жить, пора что-то менять.', Icon: LiaSmokingBanSolid, href: '/health/smoking' },
+    { title: 'Алкоголь', subtitle: 'Алкоголь является одной из самых распространенных зависимостей в мире, время освободиться от него.', Icon: PiBeerBottle, href: '/dream' },
+    { title: 'Кофе', subtitle: 'Сильный кашель, хрипы и накопление мокроты мешают нормально жить, пора что-то менять.', Icon: PiCoffee, href: '/dream' },
+    { title: 'Сладкое', subtitle: 'Избыток глюкозы, высокий уровень инсулина и лишний вес — это точно не то, что тебе нужно.', Icon: TbCandy, href: '/dream' },
+    { title: 'Наркотики', subtitle: 'Они приводят к потере контроля над собственной жизнью. Это не то, что пойдет тебе на пользу.', Icon: BiInjection, href: '/dream' },
+    { title: 'Секретная зависимость', subtitle: 'Это иллюзия, которая искажает восприятие реальных отношений и сексуальности. Время избавиться от неё.', Icon: TbRating18Plus, href: '/dream' },
   ];
 
   return (
